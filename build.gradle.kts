3b09a3f5d0d6ce76cfe98f11088287782f47e343
250king <2789260300@qq.com>
2025-06-23 03:07:56 +0800

    Fix AboutLibrary bug, update dep version, use release version lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.6.3" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "12.2.3" apply false
     id("io.sentry.android.gradle") version "5.5.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
 }
